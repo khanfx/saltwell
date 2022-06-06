49784b9e06f3f5069df9b24a90b25f47cb961df8
Fahd Khan <fahd@khanfx.com>
2022-06-06 02:15:09 +0000

    Initial commit

---
+++ src/Saltwell.tsx
@@ -0,0 +1,13 @@
+class Place {
+    public Name: string = ""
+    public Description: string = ""
+    public Items: Item[] = []
+}
+
+class Item {
+    public Name: string = ""
+    public Description: string = ""
+    public Items: Item[] = []
+}
+
+export { Place, Item } 
